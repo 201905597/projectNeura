c360711f5b7f4a518b7adf18d8e1499503d3dcb4
201905597 <78108534+201905597@users.noreply.github.com>
2021-10-15 12:11:23 +0200

    version entrega 1 FINAL

--- src/main/java/neuraHealthUI/io/IOutilities.java
+++ src/main/java/neuraHealthUI/io/IOutilities.java
@@ -7,6 +7,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class IOutilities
 {
@@ -14,8 +17,18 @@ public class IOutilities
     {
         try
         {
-            String path = "ficheros/InfoSaludMental.txt";
-            File file = new File(path);
+            //String path = "ficheros/InfoSaludMental.txt";
+            //File file = new File(path);
+            //File file = new File(IOutilities.class.getClassLoader().getResource("InfoSaludMental.txt").getFile());
+            InputStream inputStream = IOutilities.class.getClassLoader().getResourceAsStream("InfoSaludMental.txt");
+            //Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            //String fileName = file1.getName();
+            //File compiledJar = file1.getParentFile();
+            //File file = new File(compiledJar.getParentFile(), fileName);
+
+            File file = new File("temporal.txt");
+            Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
             FileReader fr = new FileReader(file, StandardCharsets.UTF_8);
             BufferedReader br = new BufferedReader(fr);
             String lineaActual = null;
@@ -31,4 +44,6 @@ public class IOutilities
             ioe.printStackTrace();
         }
     }
+
+
 }
